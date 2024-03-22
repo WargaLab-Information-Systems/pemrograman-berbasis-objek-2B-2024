f9ae760af22d35e3f6a1d0c9819c5bcafa413732
amandlyyptr <amandaaulia867@gmail.com>
2024-03-22 16:02:35 +0700

    mengumpulkan tugas kak putri

---
+++ modul-1/coba/src/coba/Coba.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package coba;
+
+/**
+ *
+ * @author AMANDA AULIA
+ */
+public class Coba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hahaha");
+    }
+    
+}
