11e38ccc8b613de16afecc12734127b194d738f7
amandlyyptr <amandaaulia867@gmail.com>
2024-03-23 18:43:45 +0700

    mengumpulkan tugas project modul 1 asprak kak putri

---
+++ modul-1/modull-1/src/modull/pkg1/manusia.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modull.pkg1;
+
+/**
+ *
+ * @author AMANDA AULIA
+ */
+public class manusia {
+    String nama,alamat;
+    int umur;
+    
+    public void berjalan () {
+        System.out.println("saya sedang berjalan");
+    }
+    public void berlari (){
+        System.out.println("sedang berlari");
+
+    }
+    public static void main(String[] args) {
+        manusia manusiaA = new manusia ();
+        manusiaA.nama = "Amanda";
+        manusiaA.umur = 18;
+        manusiaA.alamat = "Malang";
+        System.out.println("nama saya "+ manusiaA.nama);
+        System.out.println("umur saya "+ manusiaA.umur);
+        System.out.println("alamat saya "+ manusiaA.alamat);
+        manusiaA.berjalan();
+        manusiaA.berlari();
+ 
+    }
+}
