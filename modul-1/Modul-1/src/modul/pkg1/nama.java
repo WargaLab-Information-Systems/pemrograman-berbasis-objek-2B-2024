dab4b6660c41be727b550c02955087953dbe3cdd
lavelia755 <lavelliyah@gmail.com>
2024-03-23 19:19:49 +0700

    Mengumpulkan tugas modul 1 kak cindy

---
+++ modul-1/Modul-1/src/modul/pkg1/nama.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lavilia
+ */
+public class nama {
+    public static void main(String[]args){
+        Scanner input = new Scanner (System.in);
+        
+        
+        String NAMA;
+        String NIM;
+        String JURUSAN;
+        String ALAMAT;
+           
+        System.out.print("INPUT NAMA = ");
+        NAMA=input.nextLine();
+        System.out.print("INPUT NIM = ");
+        NIM=input.nextLine();
+        System.out.print("INPUT JURUSAN = ");
+        JURUSAN=input.nextLine();
+        System.out.print("INPUT ALAMAT = ");
+        ALAMAT=input.nextLine();
+        
+        
+        System.out.println("------------------------");
+        System.out.println("\nInformasi Mahasiswa : ");
+        System.out.println("Nama saya: " + NAMA);
+        System.out.println("Nim saya: " + NIM);
+        System.out.println("Jurusan saya: " + JURUSAN);
+        System.out.println("Alamat saya: " + ALAMAT);
+    }
+}
