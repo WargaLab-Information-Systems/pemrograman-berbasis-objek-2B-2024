0fc97165612bd6f56aac0c41458116e64ee3b434
lavelia755 <lavelliyah@gmail.com>
2024-03-26 10:00:46 +0700

    Mengumpulkan tugas modul 1 kak cindy

---
+++ modul-1/Modul1/src/modul1/manusia.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modull.pkg1;
+
+/**
+ *
+ * @author Lavilia
+ */
+public class manusia {
+ String nama, alamat;
+    int umur;
+    
+    public void berjalan(){
+        System.out.println("saya sedang berjalan");
+    }
+    public void berlari (){
+        System.out.println("sedang berlari");
+        
+    }
+    public static void main(String[] args) {
+        manusia manusiaA = new manusia ();
+        manusiaA.nama = "Lavelia";
+        manusiaA.umur = 18;
+        manusiaA.alamat = "yogyakarta";
+        System.out.println("nama saya "+ manusiaA.nama);
+        System.out.println("umur saya "+ manusiaA.umur);
+        System.out.println("alamat saya "+ manusiaA.alamat);
+        manusiaA.berjalan ();
+        manusiaA.berlari();
+    }
+}
+   
+
