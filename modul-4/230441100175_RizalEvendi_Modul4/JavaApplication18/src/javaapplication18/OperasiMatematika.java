b2a6b30f1a2e312f2110a5188f474873acc1f9c2
rizalevendi <rizalduro264@gmail.com>
2024-04-30 19:52:21 +0700

    23-175_Rizalevendi:kak Putri

---
+++ modul-4/230441100175_RizalEvendi_Modul4/JavaApplication18/src/javaapplication18/OperasiMatematika.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication18;
+
+/**
+ *
+ * @author M S I
+ */
+abstract class OperasiMatematika {
+    protected double a, b, c;
+
+    protected abstract void setA(double a);
+    protected abstract void setB(double b);
+    protected abstract void setC();
+    protected abstract double getA();
+    protected abstract double getB();
+    protected abstract double getC();
+    protected abstract void tampil();
+}
