4ce23a33774964c6ba83a8f1ef974c2d96957370
Riyyankusuma <143861265+Riyyankusuma@users.noreply.github.com>
2024-05-01 23:46:28 +0700

    23-164_M Riyyan Kusuma Zunaedi: Kak Cindy
    23-164_M Riyyan Kusuma Zunaedi: Kak Cindy

---
+++ 230441100164_M RIYYAN KUSUMA ZUNAEDI/opbilanganabs/Pembagian.java
@@ -0,0 +1,48 @@
+package opbilanganabs;
+
+public class Pembagian extends OPBilanganAbs implements Operasi {
+    public Pembagian() {
+        this.a = 9.25;
+        this.b = 1.25;
+    }
+
+    @Override
+    protected double get_A() {
+        return a;
+    }
+
+    @Override
+    protected double get_B() {
+        return b;
+    }
+
+    @Override
+    protected double get_C() {
+        return a / b;
+    }
+
+    @Override
+    protected void set_A(double a) {
+        this.a = a;
+    }
+
+    @Override
+    protected void set_B(double b) {
+        this.b = b;
+    }
+
+    @Override
+    protected void set_C(double c) {
+        this.c = c;
+    }
+
+    @Override
+    public void tampilHasil() {
+        System.out.println("=====");
+        System.out.println("Operasi Pembagian");
+        System.out.println("bilangan A = " + get_A());
+        System.out.println("bilangan B = " + get_B());
+        System.out.println("Hasil Pembagian A/B = " + get_C());
+        System.out.println("=====");
+    }
+}
