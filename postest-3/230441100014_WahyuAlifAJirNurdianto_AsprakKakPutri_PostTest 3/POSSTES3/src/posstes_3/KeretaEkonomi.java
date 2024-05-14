d3f90ddc78a269886e2d66dd96b620b6ebba1583
wahyualif23014 <163399059+wahyualif23014@users.noreply.github.com>
2024-05-14 20:10:18 +0700

    230441100014_WahyuAlifAjirNurdianto_AsprakKakPutri_Posttest 3

---
+++ postest-3/230441100014_WahyuAlifAJirNurdianto_AsprakKakPutri_PostTest 3/POSSTES3/src/posstes_3/KeretaEkonomi.java
@@ -0,0 +1,13 @@
+package posstes_3;
+
+public class KeretaEkonomi extends KeretaApi {
+
+    public KeretaEkonomi(String namaKereta, double jarak, String tujuan, int kapasitasPenumpang) {
+        super(namaKereta, jarak, tujuan, kapasitasPenumpang);
+    }
+
+    @Override
+    public double hitungBiaya(double hargaPerKm) {
+        return hargaPerKm * jarak;
+    }
+}
---
+++ postest-3/230441100014_WahyuAlifAJirNurdianto_AsprakKakPutri_PostTest 3/POSSTES3/src/posstes_3/Main.java
@@ -0,0 +1,19 @@
+package posstes_3;
+
+public class Main {
+    public static void main(String[] args) {
+        // Membuat objek dari berbagai jenis kereta api
+        KeretaApi eksekutif = new KeretaEksekutif("Argo Bromo", 720, "Surabaya", 300);
+        KeretaApi ekonomi = new KeretaEkonomi("Gaya Baru", 720, "Surabaya", 500);
+
+        System.out.println("=== Informasi Kereta Eksekutif ===");
+        eksekutif.tampilkanInformasiPerjalanan();
+        System.out.println("Biaya Perjalanan: " + eksekutif.hitungBiaya(10000) + " IDR");
+
+        System.out.println();
+
+        System.out.println("=== Informasi Kereta Ekonomi ===");
+        ekonomi.tampilkanInformasiPerjalanan();
+        System.out.println("Biaya Perjalanan: " + ekonomi.hitungBiaya(5000) + " IDR");
+    }
+}
