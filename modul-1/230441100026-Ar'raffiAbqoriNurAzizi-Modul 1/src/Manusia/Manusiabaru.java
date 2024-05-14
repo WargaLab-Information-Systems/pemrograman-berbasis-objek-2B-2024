90f97445e189c8f9753a1798b91f398f688094dd
230441100026-AR'RAFFI ABQORI NUR AZIZI <76115858+robet31@users.noreply.github.com>
2024-05-14 13:12:38 +0700

    230441100026-Ar'raffiAbqoriNurAzizi-Modul 1

---
+++ modul-1/230441100026-Ar'raffiAbqoriNurAzizi-Modul 1/src/Manusia/Manusiabaru.java
@@ -0,0 +1,51 @@
+package Manusia;
+
+class identitas {
+    String nama,alamat;
+    int umur;
+    long nim;
+    
+    public void nama(String nama){
+        System.out.println("Kenalin ini tuh " + nama);
+    }
+    public void umur(int umur){
+        System.out.println("Sekarang ini dia berumur " + umur);
+    }
+    public void alamat(String alamat){
+        System.out.println("Dia tuh tinggalnya di " + alamat);
+    }
+    public void berjalan(String anggotatubuh1){
+        System.out.println( "sekarang diatuh lagi " + anggotatubuh1);
+    }
+    public void berlari(String nama, String anggotatubuh2){
+        System.out.println( "ini "+ nama + " larinya tuh " + anggotatubuh2 );
+        System.out.println();
+    }
+}
+
+public class Manusiabaru {
+    
+    public static void main(String[] args){
+        identitas sihuman = new identitas();
+        identitas sihuman2 = new identitas();
+        identitas sihuman3 = new identitas();
+        
+        sihuman.nama(" Rafi ");
+        sihuman.umur(19);
+        sihuman.alamat(" Krian ");
+        sihuman.berjalan(" jalan Kaki ");
+        sihuman.berlari(" Rafi ", " lemes banget ");
+
+        sihuman2.nama(" Rapii ");
+        sihuman2.umur(19);
+        sihuman2.alamat(" Sidoarjo ");
+        sihuman2.berjalan(" santai ");
+        sihuman2.berlari(" Rapii ", " cepat banget ");
+        
+        sihuman3.nama(" Pak wowo ");
+        sihuman3.umur(34);
+        sihuman3.alamat(" Bantengan ");
+        sihuman3.berjalan(" Kaki ");
+        sihuman3.berlari(" Pak Wowo "," ngos-ngosan banget ");
+    }
+}
