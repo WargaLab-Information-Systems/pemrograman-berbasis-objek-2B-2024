2f5accd3215379ac7b40eeadc354ff842b35a74b
230441100026-AR'RAFFI ABQORI NUR AZIZI <76115858+robet31@users.noreply.github.com>
2024-05-14 11:35:01 +0700

    230441100026_Ar'raffiAbqoriNurAzizi_AsprakKakPutri_Modul 6

---
+++ modul-6/230441100026-Ar'raffiAbqoriNurAzizi-Modul6/src/modul6/BangunRuang.java
@@ -0,0 +1,12 @@
+package modul6;
+
+abstract class BangunRuang {
+    abstract double hitungVolume();
+    abstract void info();
+}
+
+interface ukuran {
+    double getJariJari();
+    double getLebar();
+    double getTinggi();
+}
