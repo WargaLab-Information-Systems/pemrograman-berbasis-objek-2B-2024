ea412d55da33d91d28feb1304011683ee840e417
silvianagitabrenda@gmail.com <silvianagitabrenda@gmail.com>
2024-05-18 20:46:14 +0700

    mengumpulkan tugas modul 3 asprak kak cindy

---
+++ modul-3/230441100152-NagitaBrendaSilvia/src/praktikum/BukuAnakAnak.java
@@ -0,0 +1,7 @@
+package praktikum;
+
+public class BukuAnakAnak extends Buku {
+    public BukuAnakAnak(String judul, String penulis, String publisher, int stok, int tahunTerbit) {
+        super(judul, penulis, publisher, "Anak-Anak", stok, tahunTerbit);
+    }
+}
---
+++ modul-3/230441100152-NagitaBrendaSilvia/src/praktikum/BukuDewasa.java
@@ -0,0 +1,8 @@
+package praktikum;
+
+public class BukuDewasa extends Buku {
+    public BukuDewasa(String judul, String penulis, String publisher, int stok, int tahunTerbit) {
+        super(judul, penulis, publisher, "Dewasa", stok, tahunTerbit);
+    }
+}
+
