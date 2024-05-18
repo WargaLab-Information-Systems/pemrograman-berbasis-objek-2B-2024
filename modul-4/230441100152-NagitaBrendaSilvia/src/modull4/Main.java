c12d62e7a55bdcb58ea8ffe1a5ae83be96009e1f
NagitaBS <silvianagitabrenda@gmail.com>
2024-05-18 19:51:59 +0700

    mengumpulkan tugas modul 4 asprak kak cindy

---
+++ modul-4/230441100152-NagitaBrendaSilvia/src/modull4/Main.java
@@ -0,0 +1,30 @@
+package modull4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        OperasiPenjumlahan operasiPenjumlahan1 = new OperasiPenjumlahan();
+        operasiPenjumlahan1.setA(9.25);
+        operasiPenjumlahan1.setB(1.25);
+        operasiPenjumlahan1.setC();
+        operasiPenjumlahan1.tampil();
+
+        OperasiPengurangan operasiPengurangan1 = new OperasiPengurangan();
+        operasiPengurangan1.setA(9.25);
+        operasiPengurangan1.setB(1.25);
+        operasiPengurangan1.setC();
+        operasiPengurangan1.tampil();
+
+        OperasiPerkalian operasiPerkalian1 = new OperasiPerkalian();
+        operasiPerkalian1.setA(9.25);
+        operasiPerkalian1.setB(1.25);
+        operasiPerkalian1.setC();
+        operasiPerkalian1.tampil();
+
+        OperasiPembagian operasiPembagian1 = new OperasiPembagian();
+        operasiPembagian1.setA(9.25);
+        operasiPembagian1.setB(1.25);
+        operasiPembagian1.setC();
+        operasiPembagian1.tampil();
+    }
+}
